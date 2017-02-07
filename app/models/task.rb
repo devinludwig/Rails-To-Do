c35d47d7d929d97ac81e1eef40582e9328776638
Devin Ludwig and Ezra Sandzer-Bell <devin@>
2017-02-07 08:51:15 -0800

    add a couple integration tests with capybara.

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,4 @@ class Task < ActiveRecord::Base
   belongs_to :list
 
   validates :description, :presence => true
-  validates :done, :presence => true
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 describe Task do
   it { should validate_presence_of :description }
   it { should belong_to :list }
-  it { should have attribute :done }
+  it { should have_attribute :done }
 end
